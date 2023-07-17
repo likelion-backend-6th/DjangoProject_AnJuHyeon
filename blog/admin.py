a7c732a37ab55c7470d5d607ef275c507fb41476
JM Bae <jungman82@gmail.com>
2023-07-17 11:42:44 +0900

    카탈로그 앱 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Post
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'post', 'created', 'active']
-    list_filter = ['active', 'created', 'updated']
-    search_fields = ['name', 'email', 'body']
+# Register your models here.
