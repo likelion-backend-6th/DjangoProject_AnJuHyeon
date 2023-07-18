dc08e0a73c56f105993a04172d67000f4b88abb6
JM Bae <jungman82@gmail.com>
2023-07-18 11:39:43 +0900

    url 패턴 작업

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,10 @@
-from django.urls import path, re_path
-from . import views
+from django.urls import path
+
+from blog import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    # post views
     path('', views.post_list, name='post_list'),
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-    re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$',
-         views.post_detail,
-         name='post_detail'),
-    path('<int:post_id>/share/',views.post_share, name='post_share'),
-    path('<int:post_id>/comment/',views.post_comment, name='post_comment'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
 ]
