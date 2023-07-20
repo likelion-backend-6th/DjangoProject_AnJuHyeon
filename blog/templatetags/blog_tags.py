2e26eeab09d6958385696eb92a2015746110fc7e
JM Bae <jungman82@gmail.com>
2023-07-20 15:33:24 +0900

    커스텀 템플릿 추가 simple_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from ..models import Post
+from blog.models import Post
 
 register = template.Library()
 
@@ -8,7 +8,3 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
-@register.inclusion_tag('blog/post/latest_posts.html')
-def show_latest_posts(count=5):
-    latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts': latest_posts}
