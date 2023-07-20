196d914d6e1e4b9ea1dc5401a5a685f965f1dae9
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-07-20 10:01:28 +0900

    카탈로그2 100pg

--- blog/admin.py
+++ blog/admin.py
@@ -4,3 +4,8 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
