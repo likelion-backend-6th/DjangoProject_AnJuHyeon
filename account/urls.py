fcc169a22415c2c46a12bc35a87a7e5ea46fff4c
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-07-31 10:14:04 +0900

    7/29

--- account/urls.py
+++ account/urls.py
@@ -10,4 +10,11 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     # 비밀번호 변경
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # 비밀번호 재설정
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
